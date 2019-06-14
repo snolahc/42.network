a8149791ea7f6b6193bd16bc803d50ecff2fbea0
Damien Michaudet <snolahc@gmail.com>
2019-06-15 01:08:24 +0200

    working dir detection

--- web/files/index.php
+++ web/files/index.php
@@ -3,6 +3,9 @@
   <head>
     <meta charset="utf-8">
     <title>action/cd</title>
+    <link rel="stylesheet" type="text/css" href="../style/colours.css">
+    <script type="text/javascript" src="../js/functions.js"></script>
+
     <style>
     body {
       background-color: #282c34;
@@ -13,39 +16,31 @@
   <body>
 
 <?php
-$json = '[{"id":3,"children":[{"id":4,"children":[{"id":5}]}]},{"id":6},{"id":2},{"id":4}]';
+$root = "data";
+$path = "/";
+$pwd = $root . $path;
+$fh = scandir($pwd);
 
-function createOLList($group) {
-    $output = (is_array($group)) ? "<ol>" : "";
-    foreach($group as $attr => $item) {
-        if(is_array($item) || is_object($item)) {
-            $output .= createOLList($item);
-        } else {
-            if($attr == "id") {
-                $output .= "<li>$item</li>";
-            }
-        }
-    }
-    $output .= (is_array($group)) ? "</ol>" : "";
-    return $output;
+if (is_writable($pwd)) {
+  echo "+ file<br />";
+  echo "+ folder<br /><br />";
 }
-
-print(createOLList(json_decode($json)));
-
+echo $path;
+echo "<ul>";
+foreach ($fh as $key => $value) {
+  if ($value == "." || ($value == ".." && $path == "/")) {
+    continue;
+  }
+  if (is_dir($pwd . $value)) {
+    $link = '?path=' . $path . $value . '&oldpwd=' . $path . '&#47;';
+    echo '<li><a href="' . $link . '">' . $value . '</a></li>';
+  } else {
+    echo "<li>" . $value . "</li>";
+  }
+}
+echo "</ul>";
  ?>
 
 
-
-    <ul>
-     <li>First</li>
-     <li>Second
-     <ul style="nested: active;list-style-type: lower-alpha; padding-bottom: 0;">
-      <li style="margin-left:2em">Sub of Second</li>
-      <li style="margin-left:2em; padding-bottom: 0;">Another Sub</li>
-    </ul>
-     </li>
-     <li>Third</li>
-     <li>Fourth </li>
-   </ul>
   </body>
 </html>
